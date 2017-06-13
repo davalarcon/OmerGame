987b3c4818cf435e3c1e4a6cde54cde489e7c858
davalarcon <zilentstorm@gmail.com>
2017-06-13 19:38:46 -0400

    update

--- app.js
+++ app.js
@@ -1,53 +1,11 @@
+var myGame = new OmerGame();
 $(document).ready(function(){
 
-function OmerGame (){
-  this.hebrewNumbers = {
-    n1To10: ['images/1.png','images/2.png','images/3.png' ],
-    n11To20: [],
-    n21To30: [],
-    n31To40: [],
-    n41To50: [],
-    };
-  this.randomCards= [];
-  this.round = 1;
-  this.currentNumber = 1;
-  this.currentCard = [];
-
-}
-
-OmerGame.prototype.startGame = (function ())
 
 
-function getRandomNumber (){
-  var randomNumber = Math.floor((Math.random()*hebrewNumbers.n1To10.length)+0);
+  myGame.startGame();
 
-  var img = hebrewNumbers.n1To10[randomNumber];
-
-  $(".randomN").attr('src', img);
-}
-
-getRandomNumber();
 
 
 
 });
-
-$('#counter').html("counter");
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var currentNumber=0;
-// while (currentNumber<50){
-//   currentNumber+=1;
-//   console.log(currentNumber);
-// }
---
+++ omer.js
@@ -0,0 +1,91 @@
+console.log('ready');
+function OmerGame (){
+  this.grid = [
+                [0,0,0],
+                [0,0,0],
+                [0,0,0],
+              ];
+  this.hebrewNumbers = ['images/1.png','images/2.png','images/3.png' ];
+  this.randomCardsGrid = '';
+  this.round = 2;
+  this.currentNumber = 1;
+  this.currentCard = '';
+  this.spotOn = '';
+  }
+$(document).ready(function(){
+
+//______ sounds_________
+    $('.start-game').click(function(){
+      ion.sound.play('glass');
+    });
+
+//_______________________
+
+    OmerGame.prototype.startGame = function (){
+      this.addRandomCards = function(){
+          for ( var i = 0 ; i <= 3; i++){
+          this.addRandomCards();
+        }
+      };
+      this.showCurrentCard();
+      this.round+=1;
+      this.currentNumber+=1;
+      };
+
+    OmerGame.prototype.nextRound = function (){
+      this.addRandomCards();
+      this.currentNumber +=1;
+      this.round +=1;
+      $('#counter').html(this.round);
+    };
+
+    OmerGame.prototype.addRandomCards = (function (){
+      var randomCards = Math.floor((Math.random()*hebrewNumbers.length)+0);
+      this.randomCardsGrid.push(this.hebrewNumbers[randomCards]);
+
+    });
+
+    OmerGame.prototype.showCurrentCard = (function(){
+    this.currentCard = (this.hebrewNumbers[this.round]);
+      var currentCardImgSrc = $(".currentCard").attr('src', this.currentCard);
+
+
+    });
+
+
+$('#counter').html(this.round);
+
+});
+
+
+
+
+//
+//
+// ion.sound({
+//     sounds: [
+//         {name: "glass"},
+//         {name: "bell_ring"},
+//         {name: "branch_break"},
+//         {name: "button_click"}
+//     ],
+//
+//     // main config
+//     path: "ion.sound-3.0.7/sounds/",
+//     preload: true,
+//     multiplay: true,
+//     volume: 0.9
+// });
+//
+// // play sound
+// ion.sound.play("beer_can_opening");
+
+// var hebrewNumbers = {
+//   n1To10: ['images/1.png','images/2.png','images/3.png' ],
+//   n11To20: [],
+//   n21To30: [],
+//   n31To40: [],
+//   n41To50: [],
+//   };
+//
+//
