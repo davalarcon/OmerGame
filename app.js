4a51a4d407f62ca0c3025d9186be8d8c05c50d94
davalarcon <zilentstorm@gmail.com>
2017-06-14 18:59:13 -0400

    80% complete
    clear

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 var myGame = new OmerGame();
 $(document).ready(function(){
+myGame.startGame();
 
+$('button').click(function(){
   myGame.startGame();
+  });
+});
 
+$(document).ready(function(){
+
+  $('.gridImg').click(function(){
+
+    if($(this).attr('src') ===  $(".currentCard").attr('src')){
+    alert('Great!');
+    myGame.nextRound();
+    }
+    else {
+      // alert('Game Over');
+      myGame.gameOver();
+    }
+  });
 
 
 
--- omer.js
+++ omer.js
@@ -1,8 +1,7 @@
-console.log('ready');
 function OmerGame (){
   this.grid = [0,0,0,0,0,0,0,0,0];
   this.hebrewNumbers = ['images/1.png','images/2.png','images/3.png','images/4.png','images/5.png','images/6.png','images/7.png','images/8.png','images/9.png','images/10.png', 'images/11.png','images/12.png','images/13.png','images/14.png','images/15.png','images/16.png','images/17.png','images/18.png','images/19.png','images/20.png','images/21.png','images/22.png','images/23.png',
-  'images/24.png','images/25.png','images/26.png','images/27.png','images/28.png','images/29.png','images/30.png' ];
+  'images/24.png','images/25.png','images/26.png','images/27.png','images/28.png','images/29.png','images/30.png','images/31.png','images/32.png','images/33.png','images/34.png','images/35.png','images/36.png','images/37.png','images/38.png','images/39.png','images/40.png', 'images/41.png','images/42.png','images/43.png','images/44.png','images/45.png','images/46.png','images/47.png','images/48.png','images/49.png','images/50.png' ];
   this.randomCardsGrid = '';
   this.currentNumber = 1;
   this.currentCard = '';
@@ -21,15 +20,15 @@ $(document).ready(function(){
       this.showCurrentCard();
       this.addRandomCards();
 
-      $('#counter').html(this.round);
+      $('#counter').html(this.currentNumber);
     };
 
 
     OmerGame.prototype.nextRound = function (){
-      this.round +=1;
+      this.currentNumber +=1;
       this.showCurrentCard();
       this.addRandomCards();
-      $('#counter').html(this.round);
+      $('#counter').html(this.currentNumber);
     };
 
     OmerGame.prototype.addRandomCards = (function (){
@@ -64,11 +63,15 @@ $(document).ready(function(){
 
     OmerGame.prototype.showCurrentCard = (function(){
     this.currentCard = (this.hebrewNumbers[this.currentNumber-1]);
-      var currentCardImgSrc = $(".currentCard").attr('src', this.currentCard);
+       currentCardImgSrc = $(".currentCard").attr('src', this.currentCard);
     });
 
+    OmerGame.prototype.gameOver = function () {
+      alert("Game over!! Try it again!!");
+      this.currentNumber = 1;
+      this.startGame();
 
-
+  };
 
 
 });
