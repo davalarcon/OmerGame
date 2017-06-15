2a94a3225573814b9c980500e05424f8b6245a12
davalarcon <zilentstorm@gmail.com>
2017-06-15 16:14:13 -0400

    fixed ion again

--- mainpage.js
+++ mainpage.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
   ion.sound({
       sounds: [
           {name: "adventurers"},
-          {name: "flowerfairyfly"}
+          {name: "flowerfairyfly.mp3"}
       ],
 
       // main config
@@ -18,7 +18,7 @@ $(document).ready(function(){
       multiplay: false,
       volume: 0.4
   });
-ion.sound.play('flowerfairyfly');
+ion.sound.play('flowerfairyfly.mp3');
 
 
 });
